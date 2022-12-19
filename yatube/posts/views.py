50339e12c37f769dccbd9232c24e25fe8c9ad80d
Антон Маренников <anton.marennikov@yandex.ru>
2022-12-19 18:40:38 +0300

    New stuff

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
+
+from .models import Post
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    return HttpResponse('А здесь будут разные посты')
+    template = 'posts/group_list.html'
+    title = "Лев Толстой – зеркало русской революции."
+    text = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context)
