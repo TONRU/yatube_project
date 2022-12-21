f9a4e0af0cc821acd4da5404214d47c141313bfc
Антон Маренников <anton.marennikov@yandex.ru>
2022-12-22 00:32:10 +0300

    Updates

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,5 +6,5 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name="main_page"),
-    path('group_list', views.group_posts, name='group_list')
+    path('group/<slug:slug>/', views.group_posts, name='group_list')
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,8 +5,10 @@ from .models import Post, Group
 
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
     context = {
         'posts': posts,
+        'title': title
     }
     return render(request, 'posts/index.html', context)
 
@@ -14,8 +16,10 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = f'Записи сообщества {group}'
     context = {
         'group': group,
         'posts': posts,
+        'title': title,
     }
     return render(request, 'posts/group_list.html', context)
